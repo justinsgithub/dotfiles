72cc43df03b6b9704dcd40390f9e418427c3f225
Justin Angeles <justinaawd@gmail.com>
2022-03-04 12:08:24 -0800

    one press tabs in normal mode, change alpha dashboard to neovim from scratch url

--- nvim/.config/nvim/lua/user/alpha.lua
+++ nvim/.config/nvim/lua/user/alpha.lua
@@ -28,7 +28,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "justintylers.com"
+	return "github.com/LunarVim/Neovim-from-scratch"
 end
 
 dashboard.section.footer.val = footer()
--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -39,6 +39,10 @@ keymap("n", "<S-h>", ":bprevious<CR>", opts)
 keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
 keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 
+-- single press indent
+keymap("n", ">", ">>", opts)
+keymap("n", "<", "<<", opts)
+
 -- Insert --
 -- Press jk fast to enter
 -- keymap("i", "jk", "<ESC>", opts)
