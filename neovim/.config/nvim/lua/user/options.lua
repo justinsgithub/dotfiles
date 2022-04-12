117311e932488f5a440e16a9085254832f0e2348
justin angeles <justinaawd@gmail.com>
2022-04-12 12:04:03 -0700

    remove neovim '-' from keywords, remove deprecated autoclose setting from nvim-tree

--- neovim/.config/nvim/lua/user/options.lua
+++ neovim/.config/nvim/lua/user/options.lua
@@ -42,7 +42,7 @@ for k, v in pairs(options) do
 end
 
 vim.cmd 'set whichwrap+=<,>,[,],h,l'
-vim.cmd [[set iskeyword+=-]]
+--vim.cmd [[set iskeyword+=-]]
 --vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
 
 -- New additions
