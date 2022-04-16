b70e014af673adc98ae7d47ebddee7f0ce3498af
justin angeles <justinaawd@gmail.com>
2022-04-16 06:55:57 -0700

    switch desktops

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,16 +1,30 @@
 local wezterm = require("wezterm")
 
+local mykeys = {}
+for i = 1, 8 do
+	-- CTRL+ALT + number to activate that tab
+	table.insert(mykeys, {
+		key = tostring(i),
+		mods = "ALT",
+		action = wezterm.action({ ActivateTab = i - 1 }),
+	})
+end
+
+table.insert(mykeys, { key = "m", mods = "ALT", action = wezterm.action({ SendString = "Hello" }) })
+table.insert(mykeys, { key = "f", mods = "ALT", action = "ToggleFullScreen" })
+table.insert(
+	mykeys,
+	{ key = "v", mods = "ALT", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) }
+)
+table.insert(
+	mykeys,
+	{ key = "h", mods = "ALT", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) }
+)
+
 return {
 	window_background_opacity = 0.95,
-  scrollback_lines = 10000,
-	--color_scheme = "Batman",
-	-- color_scheme = "Monokai Remastered",
+	scrollback_lines = 10000,
 	color_scheme = "Monokai Vivid",
 	exit_behavior = "Close",
-	keys = {
-		{ key = "m", mods = "ALT", action = wezterm.action({ SendString = "Hello" }) },
-    {key="f", mods="ALT", action="ToggleFullScreen"},
-		{ key = "v", mods = "ALT", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
-		{ key = "h", mods = "ALT", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
-	},
+	keys = mykeys,
 }
