07ec18515a59bf42705d39f08382aef63f888c47
Justin Angeles <justinaawd@gmail.com>
2023-06-24 19:10:22 -0700

    lvim config

--- lunarvim/.config/lvim/config.lua
+++ lunarvim/.config/lvim/config.lua
@@ -181,6 +181,15 @@ which_key.setup = {
       enabled = true,   -- enabling this will show WhichKey when pressing z= to select spelling suggestions
       suggestions = 20, -- how many suggestions should be shown in the list?
     },
+   presets = {
+      operators = true, -- adds help for operators like d, y, ...
+      motions = true, -- adds help for motions
+      text_objects = true, -- help for text objects triggered after entering an operator
+      windows = true, -- default bindings on <c-w>
+      nav = true, -- misc bindings to work with windows
+      z = true, -- bindings for folds, spelling and others prefixed with z
+      g = true, -- bindings for prefixed with g
+    },
   }
 }
 which_key.mappings["w"] = {
