784ebd878c462124d6cf83c88e28529b34620f99
Justin Angeles <justinaawd@gmail.com>
2023-07-01 13:08:02 -0700

    Fix nvim tree

--- lunarvim/.config/lvim/config.lua
+++ lunarvim/.config/lvim/config.lua
@@ -131,24 +131,31 @@ lvim.autocommands = {
     }
   },
 }
-
-require('delaytrain').setup{
-        delay_ms = 1000,  -- How long repeated usage of a key should be prevented
-        grace_period = 1, -- How many repeated keypresses are allowed
-        keys = {          -- Which keys (in which modes) should be delayed
-            ['nv'] = {'h', 'j', 'k', 'l'},
-            ['nvi'] = {'<Left>', '<Down>', '<Up>', '<Right>'},
-        },
-        ignore_filetypes = {}, -- Example: set to {"help", "NvimTr*"} to
-                                -- disable the plugin for help and NvimTree
-    }
 -- SETUP
+require('delaytrain').setup {
+  delay_ms = 1000,  -- How long repeated usage of a key should be prevented
+  grace_period = 1, -- How many repeated keypresses are allowed
+  keys = {
+    -- Which keys (in which modes) should be delayed
+    ['nv'] = { 'h', 'j', 'k', 'l' },
+    ['nvi'] = { '<Left>', '<Down>', '<Up>', '<Right>' },
+  },
+  ignore_filetypes = {}, -- Example: set to {"help", "NvimTr*"} to disable the plugin for help and NvimTree
+}
 pcall(function()
   require("oh-my-monokai").setup({
     transparent_background = true,
     palette = "justinsgithub",
   })
 end)
+
+
+lvim.builtin.nvimtree.setup.open_on_tab = true
+lvim.builtin.nvimtree.setup.trash = {
+      cmd = "gio trash",
+      require_confirm = true,
+}
+lvim.builtin.nvimtree.setup.view.relativenumber = true
 
 pcall(function()
   require("colorizer").setup(
